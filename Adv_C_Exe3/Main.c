86660f553c9ff84f7ffb593384ddd7a0f2861e57
Evyatar <evyatar.gen@gmail.com>
2022-04-06 14:25:56 +0300

    Forked base project and built sol. Added addToHead function. Added removeItem function. Added printList function. Added initStack function. Added destroyStack function. Added push function. Added pop function. Added isEmptyStack function. Added temporary tests for new functions in Main

--- Adv_C_Exe3/Main.c
+++ Adv_C_Exe3/Main.c
@@ -2,7 +2,22 @@
 #include "Stack.h"
 #include "Queue.h"
 
+
 void main()
 {
-	// add your code here
+	Stack Test;
+	initStack(&Test); //test initStack
+	push(&Test, 'A'); //test push + addToHead
+	push(&Test, 'B');
+	push(&Test, 'C');
+	printList(Test.head); //test printList
+	printf("\n\n==========================\n\n");
+	char tav = pop(&Test); //test pop + removeItem
+	printList(Test.head);
+	printf("\nthe tav that was poped is: %c\n", tav);
+	printf("\n\n==========================\n\n");
+	printf("before destroy: %d\n", isEmptyStack(&Test)); //test isEmptyStack
+	printf("\n\n==========================\n\n");
+	destroyStack(&Test); //test destroy
+	printf("after destroy: %d\n", isEmptyStack(&Test)); //test isEmptyStack
 }
--- Adv_C_Exe3/Stack.c
+++ Adv_C_Exe3/Stack.c
@@ -1,34 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "Stack.h"
+
+charNode* addToHead(charNode* head, charNode* toAdd);
+void removeItem(charNode** head);
+void printList(charNode* head);
+
 
 /***************** Stack ADT Implementation *****************/
 
 void initStack(Stack* s)
 {
-	// add your code here
+	s->head = NULL;
 }
 
 void destroyStack(Stack* s)
 {
-	// add your code here
+	charNode* temp;
+	while (s->head != NULL)
+	{
+		temp = s->head;
+		s->head = s->head->next;
+		free(temp);
+	}
 }
 
 void push(Stack* s, char data)
 {
-	// add your code here
+	charNode* newitem = (charNode*)malloc(sizeof(charNode));
+	if (!newitem)
+	{
+		printf("push: Memory allocation failed\n");
+		return;
+	}
+	newitem->data = data;
+	s->head = addToHead(s->head, newitem); // Recall private function 
 }
 
 char pop(Stack* s)
 {
-	// add your code here
+	char res;
+	res = s->head->data;
+	removeItem(&(s->head)); //pop current head from the stack
+	return(res);
 }
-
 
 int isEmptyStack(const Stack* s)
 {
-	// add your code here
+	if (!s->head)
+	{
+		return 1;
+	}
+	return 0;
 }
 
-/*************** Functions using stacks - Implementation/definition **************************/
+
+/*************** Functions using stacks - Implementation/definition ***************/
 
 void flipBetweenHashes(const char* sentence)
 {
@@ -44,3 +71,36 @@ void rotateStack(Stack* s, int n)
 {
 	// add your code here
 }
+
+
+/***************** Private functions for Linked List *****************/
+
+// Adds new List Item to head of list
+charNode* addToHead(charNode* head, charNode* toAdd)
+{
+	toAdd->next = head;
+	head = toAdd;
+	return head;
+}
+
+// Deletes the first List Item from list
+void removeItem(charNode** head)
+{
+	if (*head == NULL) return; //if stack empty
+	charNode* temp = *head;
+	*head = (*head)->next;
+	free(temp);
+}
+
+// Prints Linked List
+void printList(charNode* head)
+{
+	charNode* temp;
+	temp = head;
+	while (temp != NULL)
+	{
+		printf("%c\n", temp->data);
+		temp = temp->next;
+	}
+}
+
